33215998ee27a1804ebe4b0e8f5e6b7fe5753ded
abenezer <54418157+Abenezer-Daniel-W@users.noreply.github.com>
2023-12-31 17:14:05 -0700

    initial

---
+++ src/chessEngine/board.java
@@ -0,0 +1,5 @@
+package chessEngine;
+
+public class board extends JPanel{
+
+}
---
+++ src/chessEngine/game.java
@@ -0,0 +1,17 @@
+package chessEngine;
+
+import javax.swing.JFrame;
+
+public class game {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame game = new JFrame();
+
+		game.setSize(800, 800);  
+		game.setLocationRelativeTo(null);  
+		game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		game.setVisible(true);  
+	}
+
+}
